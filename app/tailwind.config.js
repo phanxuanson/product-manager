a1e7cf9be44571f3e961611fe603248ca1fb3e9d
Son Phan <son.phan@asnet.com.vn>
2025-07-10 17:32:34 +0700

    feat: initial project setup

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          primary: ['Open Sans', 'sans-serif'],
+          tertiary: ['Montserrat', 'sans-serif'],
+          secondary: ['Regular', 'sans-serif'],
+        },
+        lineHeight: {
+
+        },
+        colors: {
+          primary: '#00072B',
+          secondary: '#7FC008',
+          tertiary: '#DB303F',
+          quaternary: '#303030',
+          neutral: '#475467',
+          warning: '#DB303F',
+        },
+      },
     },
     plugins: [],
   }
