bdf70ddc9fe00ce881649e822e24766700158b83
Son Phan <son.phan@asnet.com.vn>
2025-08-01 10:13:49 +0700

    rename: regex file to authValidators file

--- management-product/src/scripts/login.js
+++ management-product/src/scripts/login.js
@@ -1,4 +1,4 @@
-import { isValidEmail, isValidPassword } from './utils/regex.js';
+import { isValidEmail, isValidPassword } from './utils/authValidators.js';
 import { MESSAGES } from './constants/messages.js';
 import { USER_DATA } from './constants/user-data.js';
 
