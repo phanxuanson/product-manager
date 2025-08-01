8198fb71dd2f67478a2a0c7edb93e0e0d8ab9411
Son Phan <son.phan@asnet.com.vn>
2025-08-01 10:19:03 +0700

    rename: regex to validate

--- management-product/src/scripts/login.js
+++ management-product/src/scripts/login.js
@@ -1,4 +1,4 @@
-import { isValidEmail, isValidPassword } from './utils/authValidators.js';
+import { isValidEmail, isValidPassword } from './utils/validate.js';
 import { MESSAGES } from './constants/messages.js';
 import { USER_DATA } from './constants/user-data.js';
 
