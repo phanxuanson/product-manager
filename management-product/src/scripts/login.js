8d43dcc6f48e8880946839f5f2d1feb4fc3e3641
Son Phan <son.phan@asnet.com.vn>
2025-08-11 16:36:45 +0700

    fix: move user data file to mocks folder

--- management-product/src/scripts/login.js
+++ management-product/src/scripts/login.js
@@ -1,6 +1,6 @@
 import { isValidEmail, isValidPassword } from './utils/validation.js';
 import { MESSAGES } from './constants/messages.js';
-import { USER_DATA } from './utils/user-data.js';
+import { USER_DATA } from './mocks/user-data.js';
 
 class LoginForm {
   constructor(formSelector) {
